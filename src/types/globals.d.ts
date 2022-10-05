c415746a42b0f9096ac1e3bc3f58f98c76993f61
Scotty Jamison <theScottyJam@gmail.com>
2022-10-04 21:48:38 -0600

    Remove uneeded type definitions

--- src/types/globals.d.ts
+++
@@ -1,8 +0,0 @@
-import type { validatable } from '../validatableProtocol';
-import type { ValidatableProtocolFn } from './validatableProtocol';
-
-declare global {
-  interface Function {
-    [validatable]: ValidatableProtocolFn
-  }
-}
