c71f7d6d68372e785973a300d01bea72c5e8052e
Scotty Jamison <theScottyJam@gmail.com>
2023-04-20 07:37:57 -0600

    Update version to 0.0.6

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import type { Validator } from './types/validator';
+import type { Validator, Expectation } from './types/validator';
 import type { FrozenMap as FrozenMapClass } from './util';
 import { ValidatorAssertionError, ValidatorSyntaxError } from './exceptions';
 import { validator } from './validatorApi';
 
 export { ValidatorAssertionError, ValidatorSyntaxError };
 export * from './types/parsingRules';
-export type { Validator };
+export type { Validator, Expectation };
 export type FrozenMap<K, V> = InstanceType<typeof FrozenMapClass>;
 export { validator };
